1e685d35e43e02f22003b8c13b4da211aa7ec10a
Adrian <adrian@soft4you.com.pl>
2021-03-02 13:11:00 +0100

    pierwszy commit

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
-Witaj!
+ <link rel="stylesheet" href="css/bootstrap.css"/>
+
+
+<div>
+    <h2>
+        Witaj w aplikacji bankowej!
+    </h2>
+</div>
+
+<button class="btn btn-outline-primary">asddas</button>
